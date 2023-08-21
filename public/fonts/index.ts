f704b291a05660b2e68b7e5f0e96fbeb7ab38846
MINHEE <tigerleo7@naver.com>
2023-08-21 15:14:41 +0900

    chore: 폰트 설정

---
+++ public/fonts/index.ts
@@ -0,0 +1,5 @@
+import { Inter, Noto_Sans_KR } from "next/font/google";
+
+export { pretendard } from "./pretendard";
+export const inter = Inter({ subsets: ["latin"] });
+export const notoSans = Noto_Sans_KR({ weight: "400", subsets: ["latin"] });
