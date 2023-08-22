18518ccbcd40a5ff3c902fd6d8c79b610e9b2b70
MINHEE <tigerleo7@naver.com>
2023-08-22 10:58:45 +0900

    feat: Icon 컴포넌트

---
+++ components/atoms/Icon.tsx
@@ -0,0 +1,37 @@
+import {
+  ExperimentFilled,
+  FileUnknownOutlined,
+  HomeFilled,
+  HomeOutlined,
+  LoadingOutlined,
+} from "@ant-design/icons";
+import { CSSProperties } from "react";
+
+export type IconIdType = "loading" | "home" | "test" | "unknown";
+
+const Icon = ({
+  iconId = "unknown",
+  type = "filled",
+  ...props
+}: {
+  iconId: IconIdType;
+  type?: "outlined" | "filled";
+  spin?: boolean;
+  rotate?: number;
+  className?: string;
+  style?: CSSProperties;
+}) => {
+  if (iconId === "home")
+    return type === "outlined" ? (
+      <HomeOutlined {...props} />
+    ) : (
+      <HomeFilled {...props} />
+    );
+
+  if (iconId === "test") return <ExperimentFilled {...props} />;
+  if (iconId === "loading") return <LoadingOutlined {...props} />;
+
+  return <FileUnknownOutlined {...props} />;
+};
+
+export default Icon;
