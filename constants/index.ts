8038e6c8af827da4cbf36f3082ed7732d2e6b768
MINHEE <tigerleo7@naver.com>
2023-08-22 10:59:08 +0900

    feat: MAIN_MENUS 목록 정의

---
+++ constants/index.ts
@@ -0,0 +1,18 @@
+import { IconIdType } from "@/components/atoms/Icon";
+
+export const MAIN_MENUS: Array<{
+  name: string;
+  url: string;
+  iconId: IconIdType;
+}> = [
+  {
+    name: "Home",
+    url: "/",
+    iconId: "home",
+  },
+  {
+    name: "Test",
+    url: "/test",
+    iconId: "test",
+  },
+];
