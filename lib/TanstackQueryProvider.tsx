86d116238aa126ff16ed165e9c98d4e5edc94f68
MINHEE <tigerleo7@naver.com>
2023-08-24 13:53:34 +0900

    chore: tanstack query 관련 설정

---
+++ lib/TanstackQueryProvider.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { ReactNode, useState } from "react";
+import {
+  DehydratedState,
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+export default function Providers({
+  children,
+  dehydratedState,
+}: {
+  children: ReactNode;
+  dehydratedState?: DehydratedState;
+}) {
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={dehydratedState}>{children}</HydrationBoundary>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
