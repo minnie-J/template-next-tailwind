9433af5fe29f882303b5736136651cccdf131198
MINHEE <tigerleo7@naver.com>
2023-08-25 16:47:26 +0900

    feat: test 하위 페이지

--- app/test/[id]/page.tsx
+++ app/test/[id]/page.tsx
@@ -1,13 +1,61 @@
 "use client";
 
+import { useState } from "react";
 import { useParams } from "next/navigation";
+import { useQuery } from "@tanstack/react-query";
+import classNames from "classnames";
 
-import Wireframe from "@/components/atoms/Wireframe";
+import { listSub } from "@/apis";
+import { DETAIL_MENUS } from "@/constants";
 
-const Page = () => {
+import SplitLayout from "@/components/templates/SplitLayout";
+import SubMenuItem from "@/components/atoms/SubMenuItem";
+
+const tabKeys = DETAIL_MENUS.map(({ key }) => key);
+type TabType = (typeof tabKeys)[number];
+
+const TestDetailPage = () => {
   const { id } = useParams();
-  console.log("🚀 ~ file: page.tsx:8 ~ Page ~ id:", id);
-  return <Wireframe>{`ID: ${id}`}</Wireframe>;
+
+  const [currentTab, setCurrentTab] = useState<TabType>("sub1");
+
+  const { data, isLoading } = useQuery<Array<{ id: string; name: string }>>({
+    queryKey: ["subData", { id, tab: currentTab }],
+    queryFn: () => listSub({ id: `${id}`, tab: currentTab }),
+  });
+
+  const { Element } = SplitLayout;
+  return (
+    <SplitLayout direction="vertical">
+      <Element height={50}>
+        <div
+          className={classNames(
+            "flex",
+            "grow",
+            "h-full",
+            "bg-gray-200",
+            "text-gray-400",
+          )}
+        >
+          {DETAIL_MENUS.map(({ name, key }) => (
+            <SubMenuItem
+              key={key}
+              isActive={currentTab === key}
+              onClick={() => setCurrentTab(key)}
+            >
+              {name}
+            </SubMenuItem>
+          ))}
+        </div>
+      </Element>
+      <Element>
+        <div className={classNames("flex", "flex-col", "border-l")}>
+          {data?.length != null &&
+            data?.map(({ id, name }) => <div key={id}>{name}</div>)}
+        </div>
+      </Element>
+    </SplitLayout>
+  );
 };
 
-export default Page;
+export default TestDetailPage;
