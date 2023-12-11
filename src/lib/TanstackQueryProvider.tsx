2e05a6d1dbb453f47db918d3b28b5308467dc85e
MINHEE <tigerleo7@naver.com>
2023-12-11 15:40:21 +0900

    feat(TanstackQueryProvider): Suspense(streaming) 적용을 위한 wrapper 추가

--- src/lib/TanstackQueryProvider.tsx
+++ src/lib/TanstackQueryProvider.tsx
@@ -2,6 +2,7 @@
 
 import { ReactNode, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryStreamedHydration } from "@tanstack/react-query-next-experimental";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function Providers({ children }: { children: ReactNode }) {
@@ -17,12 +18,12 @@ export default function Providers({ children }: { children: ReactNode }) {
             gcTime: 60 * 1000,
           },
         },
-      }),
+      })
   );
 
   return (
     <QueryClientProvider client={queryClient}>
-      {children}
+      <ReactQueryStreamedHydration>{children}</ReactQueryStreamedHydration>
       <ReactQueryDevtools />
     </QueryClientProvider>
   );
