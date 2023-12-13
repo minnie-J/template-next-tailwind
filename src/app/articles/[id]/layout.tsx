cc2ae9f77c18da2e1f1894e5acae3f3e8313d605
MINHEE <tigerleo7@naver.com>
2023-12-13 15:43:56 +0900

    feat: article 컴포넌트 레이아웃 구성

--- src/app/articles/[id]/layout.tsx
+++ src/app/articles/[id]/layout.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { ReactNode } from "react";
+import classNames from "classnames";
 
-const ArticlesLayout = () => {
-  return <div>layout</div>;
+import SplitLayout from "@/components/templates/SplitLayout";
+
+const ArticleLayout = ({
+  children,
+  comments,
+  articlesByAuthor,
+}: {
+  children: ReactNode;
+  comments: ReactNode;
+  articlesByAuthor: ReactNode;
+}) => {
+  return (
+    <div className={classNames("flex", "grow", "justify-center")}>
+      <main className={classNames("flex", "py-16")}>
+        <SplitLayout direction="vertical">
+          <article className={classNames("w-[44rem]", "min-h-[38rem]")}>
+            {children}
+          </article>
+          <section>{comments}</section>
+        </SplitLayout>
+        <aside className={classNames("w-80")}>{articlesByAuthor}</aside>
+      </main>
+    </div>
+  );
 };
 
-export default ArticlesLayout;
+export default ArticleLayout;
