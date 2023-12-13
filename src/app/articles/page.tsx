fc359231dd9666e25264bab3e6ac1c8b2d17dd23
MINHEE <tigerleo7@naver.com>
2023-12-13 15:44:28 +0900

    feat: articles 컴포넌트에서 article 컴포넌트 오픈을 위한 임시 링크 적용

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,10 +1,17 @@
+"use client";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import classNames from "classnames";
 
 const ArticlesPage = () => {
+  const pathname = usePathname();
   return (
     <div className={classNames("flex", "flex-grow", "flex-col")}>
       <div>title area</div>
-      <div>list area</div>
+      <ul>
+        <Link href={`${pathname}/1`}>go to article</Link>
+      </ul>
     </div>
   );
 };
